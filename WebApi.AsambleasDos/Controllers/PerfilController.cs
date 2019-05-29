d4152e7aedc2963a782c3f838cc72b0226d86663
vcoronado <vcoronado@rayensalud.com>
2019-05-29 17:23:28 -0400

    nuevos controller

---
+++ WebApi.AsambleasDos/Controllers/PerfilController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VCFramework.Entidad;
+using VCFramework.NegocioMySQL;
+using System.Web.Http.Cors;
+using Newtonsoft.Json;
+using System.Xml;
+using System.Net.Http.Formatting;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Text;
+using System.Web;
+using System.Net.Mail;
+
+namespace WebApi.AsambleasDos.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class PerfilController : ApiController
+    {
+        const string UploadDirectory = "~/Repositorio/";
+        const string UploadDirectoryImg = "~/apps/img/";
+        [HttpPost]
+        public HttpResponseMessage UploadFile()
+        {
+            //if (HttpContext.Current.Request.Files.AllKeys.Any())
+            //{
+            // Get the uploaded image from the Files collection
+            var httpPostedFile = HttpContext.Current.Request.Files["UploadedImage"];
+            string input = HttpContext.Current.Request.Form["perfil"];
+
+            string[] inputs = input.Split(',');
+            //string Input = JsonConvert.SerializeObject(ppp);
+
+            dynamic data = JObject.Parse(input);
+
+            string id = data.Id;
+            if (id == null)
+                id = "0";
+            int idBuscar = int.Parse(id);
+
+            string esCpasStr = "false";
+            bool esCpas = false;
+            if (data.EsCpas != null)
+            {
+                esCpasStr = data.EsCpas;
+                esCpas = Convert.ToBoolean(esCpasStr);
+            }
+
+            //validaciones antes de ejecutar la llamada.
+            VCFramework.Entidad.MiPerfil aus = VCFramework.NegocioMySql.PrefilUsuario.BuscarPorId(idBuscar);
+            VCFramework.Entidad.MiPerfil entidad = new VCFramework.Entidad.MiPerfil();
+            HttpResponseMessage httpResponse = new HttpResponseMessage();
+
+            try
+            {
+                string iniciales = data.Iniciales;
+                string apodo = data.Apodo;
+                string ausId = data.AusId;
+                string nombreArchivo = data.Foto;
+                string archivoGuardar = "";
+                if (httpPostedFile != null && httpPostedFile.FileName != null)
+                {
+                    archivoGuardar = httpPostedFile.FileName;
+                }
+                else
+                {
+                    archivoGuardar = "#";
+                }
+                //quitar //
+                string[] pedazos = nombreArchivo.Split('\\');
+                string nom = pedazos[pedazos.Length - 1].ToString();
+
+                if (aus == null)
+                    aus = new VCFramework.Entidad.MiPerfil();
+
+                if (aus != null)
+                {
+
+                    int nuevoId = 0;
+
+                    entidad.Apodo = apodo;
+                    entidad.Iniciales = iniciales;
+                    entidad.AusId = int.Parse(ausId);
+
+
+                    if (aus.Id == 0)
+                    {
+                        nuevoId = VCFramework.NegocioMySql.PrefilUsuario.Insertar(entidad);
+                    }
+                    else
+                    {
+                        nuevoId = aus.Id;
+                        entidad.Id = nuevoId;
+                        VCFramework.NegocioMySql.PrefilUsuario.Actualizar(entidad);
+                    }
+
+
+                    httpResponse = new HttpResponseMessage(HttpStatusCode.OK);
+                    String JSON = JsonConvert.SerializeObject(entidad);
+                    httpResponse.Content = new StringContent(JSON);
+                    httpResponse.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(VCFramework.NegocioMySQL.Utiles.JSON_DOCTYPE);
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                httpResponse = new HttpResponseMessage(HttpStatusCode.ExpectationFailed);
+                throw ex;
+            }
+
+
+            if (httpPostedFile != null)
+            {
+
+                // Validate the uploaded image(optional)
+
+                // Get the complete file path
+                var fileSavePath = Path.Combine(HttpContext.Current.Server.MapPath("~/apps/Repositorio"), httpPostedFile.FileName);
+
+                // Save the uploaded file to "UploadedFiles" folder
+                httpPostedFile.SaveAs(fileSavePath);
+            }
+
+            return httpResponse;
+        }
+    }
+}
---
+++ WebApi.AsambleasDos/Controllers/PerfilUsuarioController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VCFramework.Entidad;
+using VCFramework.NegocioMySQL;
+using System.Web.Http.Cors;
+using Newtonsoft.Json;
+using System.Xml;
+using System.Net.Http.Formatting;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Text;
+using System.Web;
+using System.Globalization;
+using System.Net.Mail;
+
+namespace WebApi.AsambleasDos.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class PerfilUsuarioController : ApiController
+    {
+        [AcceptVerbs("OPTIONS")]
+        public void Options()
+        { }
+
+        [AgendaExceptionFilter]
+        [System.Web.Http.AcceptVerbs("GET")]
+        [HttpGet]
+        public HttpResponseMessage Get([FromUri]string id)
+        {
+
+            //validaciones antes de ejecutar la llamada.
+            if (id == "")
+                throw new ArgumentNullException("Id");
+
+
+            HttpResponseMessage httpResponse = new HttpResponseMessage();
+            try
+            {
+                //string instId = data.InstId;
+                int idBuscar = int.Parse(id);
+
+                VCFramework.Entidad.MiPerfil miperfil = VCFramework.NegocioMySql.PrefilUsuario.BuscarPorId(idBuscar);
+
+
+                if (miperfil != null)
+                {
+
+                    httpResponse = new HttpResponseMessage(HttpStatusCode.OK);
+                    String JSON = JsonConvert.SerializeObject(miperfil);
+                    httpResponse.Content = new StringContent(JSON);
+                    httpResponse.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(VCFramework.NegocioMySQL.Utiles.JSON_DOCTYPE);
+                }
+                else
+                {
+                    httpResponse = new HttpResponseMessage(HttpStatusCode.NoContent);
+                }
+                //Uri uri = new Uri(AgendaWeb.Integracion.Utils.ObtenerUrlLogin());
+
+                //httpResponse = AgendaWeb.Integracion.PostResponse.GetResponse(uri, Input);
+            }
+            catch (Exception ex)
+            {
+                httpResponse = new HttpResponseMessage(HttpStatusCode.ExpectationFailed);
+                throw ex;
+            }
+            return httpResponse;
+        }
+
+        [System.Web.Http.AcceptVerbs("DELETE")]
+        public HttpResponseMessage Delete(dynamic DynamicClass)
+        {
+
+            string Input = JsonConvert.SerializeObject(DynamicClass);
+
+            dynamic data = JObject.Parse(Input);
+
+            //validaciones antes de ejecutar la llamada.
+            if (data.Id == 0)
+                throw new ArgumentNullException("Id");
+
+
+            HttpResponseMessage httpResponse = new HttpResponseMessage();
+
+            try
+            {
+                string id = data.Id;
+                int idBuscar = int.Parse(id);
+
+                string esCpasStr = "false";
+                bool esCpas = false;
+                if (data.EsCpas != null)
+                {
+                    esCpasStr = data.EsCpas;
+                    esCpas = Convert.ToBoolean(esCpasStr);
+                }
+
+                VCFramework.Entidad.MiPerfil inst = VCFramework.NegocioMySql.PrefilUsuario.BuscarPorId(idBuscar);
+
+                if (inst != null && inst.Id > 0)
+                {
+
+                    VCFramework.NegocioMySql.PrefilUsuario.Eliminar(inst);
+
+
+                    httpResponse = new HttpResponseMessage(HttpStatusCode.OK);
+                    String JSON = JsonConvert.SerializeObject(inst);
+                    httpResponse.Content = new StringContent(JSON);
+                    httpResponse.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(VCFramework.NegocioMySQL.Utiles.JSON_DOCTYPE);
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                httpResponse = new HttpResponseMessage(HttpStatusCode.ExpectationFailed);
+                throw ex;
+            }
+            return httpResponse;
+
+        }
+        [System.Web.Http.AcceptVerbs("POST")]
+        public HttpResponseMessage Post(dynamic DynamicClass)
+        {
+
+            string Input = JsonConvert.SerializeObject(DynamicClass);
+
+            dynamic data = JObject.Parse(Input);
+
+
+            string buscarNombreUsuario = "";
+            if (data.BuscarId != null)
+            {
+                buscarNombreUsuario = data.BuscarId;
+            }
+
+            //validaciones antes de ejecutar la llamada.
+            if (data.AusId == 0)
+                throw new ArgumentNullException("AusId");
+
+
+            HttpResponseMessage httpResponse = new HttpResponseMessage();
+            try
+            {
+                string ausId = data.AusId;
+                int instIdBuscar = int.Parse(ausId);
+
+                VCFramework.Entidad.MiPerfil perfil = VCFramework.NegocioMySql.PrefilUsuario.BuscarPorAusId(instIdBuscar);
+
+
+                httpResponse = new HttpResponseMessage(HttpStatusCode.OK);
+                String JSON = JsonConvert.SerializeObject(perfil);
+                httpResponse.Content = new StringContent(JSON);
+                httpResponse.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(VCFramework.NegocioMySQL.Utiles.JSON_DOCTYPE);
+            }
+            catch (Exception ex)
+            {
+                httpResponse = new HttpResponseMessage(HttpStatusCode.ExpectationFailed);
+                throw ex;
+            }
+            return httpResponse;
+
+
+        }
+
+
+        [System.Web.Http.AcceptVerbs("PUT")]
+        public HttpResponseMessage Put(dynamic DynamicClass)
+        {
+            return new HttpResponseMessage(HttpStatusCode.MethodNotAllowed);
+        }
+    }
+}
